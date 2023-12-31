3d9538cc208c4f5185e44a0270ed701c6713105e
abdullah <ab15021442@gmail.com>
2023-12-31 14:42:01 +0500

    Dockerize ML Model

--- model-training/training.py
+++ model-training/training.py
@@ -32,7 +32,7 @@ for k in range_of_k :
             print("score:",score)
             best_model = trained_model
             best_silhouette_score = score
-            model_path = f"models/kmeans_model.pkl"
+            model_path = f"kmeans_model.pkl"
             joblib.dump(trained_model, model_path)
         mlflow.end_run()
 if best_model is not None:
